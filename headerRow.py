0253d8de6405aecb1cd728db9e7d2119a4a1cec9
wiegs <jason.philip.wiegand@gmail.com>
2022-12-10 08:15:29 -0500

    Update headerRow.py
    I fixed the variable I forgot to initialize.

--- headerRow.py
+++ headerRow.py
@@ -1,4 +1,6 @@
 # write the output file header row, close it after
+csv_output = "processJSON-RESULTS.csv"      # initialize variables 
+
 with open (csv_output, "a") as outputfile:   
     outputfile.write("Domain;IP;DNS;ASN;Registrar;Created;Expires;FQDNs\n")
 outputfile.close()
