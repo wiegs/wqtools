5e8923f2e5da23eae69bd47699a8210a4ee587c3
wiegs <jason.philip.wiegand@gmail.com>
2022-12-10 08:37:43 -0500

    Create processLRL.sh
    I added a new file and comment line.

---
+++ processLRL.sh
@@ -0,0 +1,4 @@
+#!/bin/bash                               # Go through a list of all your domains to process
+python processLRL.py domain1_report.json
+python processLRL.py domain2_report.json
+python processLRL.py domain3_report.json
