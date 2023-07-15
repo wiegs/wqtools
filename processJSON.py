bb9c83a1f22a33740a129618057d66964eb75e41
wiegs <jason.philip.wiegand@gmail.com>
2023-07-15 15:39:47 -0400

    Update processJSON.py
    added support for new format output, the subdomains no longer list but gives the number of them

--- processJSON.py
+++ processJSON.py
@@ -1,18 +1,9 @@
-#processJSON.py
-#==============
+#processLRL.py
+#=============
 #
-#Takes the saved output from Lost Rabbit Labs' WisQuas Digital Footprint Discovery and Inventory
-# and puts it into a CSV file to create a spreadsheet to share with your webmaster or marketing
+#process the output from Lost Rabbit Labs' WisQuas JSON data into CSV
 #
-# INPUT: .json report file
-# OUTPUT: saves a row to the CSV file and gives the OK sign
-#
-# ARGUMENTS: 1 - the input filename
-#
-# note CSV file uses semicolon ";" separator
-#      if you want a header run headerRow.py
-#
-# EXAMPLE: python processJSON.py report.json
+#Args: one(1): the input filename
 #
 import sys                              
 import os
@@ -27,6 +18,8 @@ theRegistrar = ""
 theCreated = ""
 theExpires = ""
 theFQDNs = ""
+theFortiguard = ""                          # variable home for Fortiguard site rating
+theSiteadvisor = ""                         # variable home for Siteadvisor site rating
 JSONfilename = sys.argv[1].strip()          # strip out stuff like whitespace probably
 JSONfilename = JSONfilename.strip('"')      # strip out doublequotes for some reason
 with open(JSONfilename) as f:               # load the file
@@ -38,45 +31,50 @@ for x in keys:                              # loop through the keys array: query
         theDomain = json_var[x]             # we load the domain into a variable to use at the end for the output
 # go get the rest from results 
 for x in keys:
-    if x == "results":
+    if x == "scanDoc":
         json_result = json_var[x]
         try:
-            theIP = json_result[0]["topScan"]["scan"]["ipaddr"]
+            theIP = json_result["ipaddr"]
         except:
             theIP = ""
         try:
-            theDNS = json_result[0]["topScan"]["scan"]["whois"]["nameservers"]
+            theDNS = json_result["whois"]["nameservers"]
         except:
             theDNS = ""    
         theDNS = str(theDNS)
         try:
-            theASN = json_result[0]["topScan"]["scan"]["ip"]["asn"]    
+            theASN = json_result["ipwhois"]["asn"]    
         except:
             theASN = ""
         try:
-            theASN = theASN + " " + json_result[0]["topScan"]["scan"]["ip"]["asn_description"]
+            theASN = "ASN " + theASN + " " + json_result["ipwhois"]["asn_description"]
         except:
             theASN = theASN + ""       
         try:
-            theRegistrar = json_result[0]["topScan"]["scan"]["whois"]["registrar"]
+            theRegistrar = json_result["whois"]["registrar"]
         except:
             theRegistrar = ""
         try:
-            theCreated = json_result[0]["topScan"]["scan"]["whois"]["created_date"]
+            theCreated = json_result["whois"]["created_date"]
         except:
             theCreated = ""
         try:    
-            theExpires = json_result[0]["topScan"]["scan"]["whois"]["expired_date"]
+            theExpires = json_result["whois"]["expired_date"]
         except:
             theExpires = ""
         try:
-            theFQDNs = json_result[0]["topScan"]["scan"]["subdomains"]
+            theSiteadvisor = json_result["blacklist"]["siteadvisor"]
+        except:
+            theSiteadvisor = ""
+        try:
+            theFQDNs = json_result["subdomains"]
         except:
             theFQDNs = ""
         theFQDNs = str(theFQDNs)
 
 with open (csv_output, "a") as outputfile:  # write a new line to the output file
-    outputfile.write(theDomain + ";" + theIP +";" + theDNS +";" + theASN +";" + theRegistrar +";" + theCreated +";" + theExpires +";" + theFQDNs +"\n")
+    outputfile.write(theDomain + ";" + theIP +";" + theDNS +";" + theASN +";" + theRegistrar +";" + theCreated +";" + theExpires +";" + theSiteadvisor +";" + theFQDNs +"\n")
 outputfile.close()                          # close the output file
 
 print(sys.argv[1] + " processed OK!")       # give the OK sign
+
