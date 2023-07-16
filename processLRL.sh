ff248a8162b0f3888dede4d81d86f8d88094c9e5
wiegs <jason.philip.wiegand@gmail.com>
2023-07-16 17:36:46 -0400

    Update processLRL.sh

--- processLRL.sh
+++ processLRL.sh
@@ -1,4 +1,4 @@
 #!/bin/bash                               # Go through a list of all your domains to process
-python processLRL.py domain1_report.json
-python processLRL.py domain2_report.json
-python processLRL.py domain3_report.json
+python processJSON.py domain1_report.json
+python processJSON.py domain2_report.json
+python processJSON.py domain3_report.json
