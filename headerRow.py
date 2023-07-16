a9a2863ff571c358cb7366a68a8833f96766f93c
wiegs <jason.philip.wiegand@gmail.com>
2023-07-15 23:39:29 -0400

    Update headerRow.py

--- headerRow.py
+++ headerRow.py
@@ -1,6 +1,6 @@
 # write the output file header row, close it after
-csv_output = "processJSON-RESULTS.csv"      # initialize variables 
+CSV_OUTPUT = "processJSON-RESULTS.csv"      # initialize variables 
 
-with open (csv_output, "a") as outputfile:   
+with open (CSV_OUTPUT, "a") as outputfile:
     outputfile.write("Domain;IP;DNS;ASN;Registrar;Created;Expires;Siteadvisor;FQDNs;theDataLastUpdated;\n")
 outputfile.close()
