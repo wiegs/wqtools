49347cf4ecb794fbe174db9e38a7f5c1c16813f2
wiegs <jason.philip.wiegand@gmail.com>
2023-07-16 17:32:14 -0400

    Update setupEnv.sh

--- setupEnv.sh
+++ setupEnv.sh
@@ -1,2 +1,2 @@
 # load the API Key from 1Password CLI - do "op singin" command first
-export WQ_API_KEY="$(op read "op:/VaultName/SecretName/password")"
+export WQ_API_KEY="$(op read "op://VaultName/SecretName/password")"
